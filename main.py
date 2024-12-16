039e96d829dacb1557a39992245e02bbde6938db
AlexAb123 <alex.abdulhayoglu@gmail.com>
2024-12-16 17:48:22 -0500

    c

--- main.py
+++ main.py
@@ -1,62 +1,45 @@
-import curses
-from utils.colors import initialize_colors
-from utils.helpers import create_buttons
-from aoc_2024.day_02 import visualize_2024_02
-from aoc_2024.day_12 import visualize_2024_12
+import pygame
 
+
+from utils.colors import BUTTON_COLOR, BUTTON_HOVER_COLOR
+
+
+
+""" from aoc_2024.day_02 import visualize_2024_02
+from aoc_2024.day_12 import visualize_2024_12
 visualizations_2024 = [visualize_2024_02, visualize_2024_12]
 visualizations = {
     2024: visualizations_2024
 }
-year = 2024
+year = 2024 """
+
+def draw_main_menu():
+    pass
 
 
-def exit_visualizer():
-    curses.endwin()
-    exit()
+def main():
+
+    pygame.init()
+    WIDTH, HEIGHT = 800, 600
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+         
+
+            if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_a:
+                    running = False
+
+            elif event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.update()
 
 
-
-def back(stdscr: curses.window):
-    print("HI")
-def day_select_screen(stdscr: curses.window):
-    selected_button = 0
-
-    stdscr.clear()
-    curses.resize_term(20, 20)
-
-    # Initialize buttons
-    button_texts = ["[Back]"]
-    functions = [back]
-    for visualization in visualizations[year]:
-        button_texts.append("[Day"+visualization.__name__.split("_")[-1]+"]")
-        functions.append(visualization)
-
-    buttons = create_buttons(stdscr, 0, 0, button_texts, functions)
-
-    while True:
-        stdscr.clear()
-        for i, button in enumerate(buttons):
-            button.draw(selected_button==i)
-
-        key = stdscr.getkey().upper()
-        if key == "KEY_UP" or key == "W":
-            selected_button = (selected_button - 1) % len(buttons)
-        elif key == "KEY_DOWN" or key == "S":
-            selected_button = (selected_button + 1) % len(buttons)
-        elif key == "\n":
-            buttons[selected_button].function(stdscr)
-        stdscr.refresh()
-
-
-
-def main(stdscr: curses.window):
-    initialize(stdscr)
-
-    day_select_screen(stdscr)
-
-def initialize(stdscr: curses.window):
-    curses.curs_set(0)
-    initialize_colors()
-
-curses.wrapper(main)
+if __name__ == "__main__":
+    main()
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,20 +1,2 @@
-import curses
-from utils.button import Button
-from collections.abc import Callable
-
-def create_buttons(stdscr: curses.window, start_row: int, col: int, texts: list[str], functions: list[Callable]) -> list[Button]:
-    buttons: list[Button] = []
-    for i, text in enumerate(texts):
-        buttons.append(Button(stdscr, start_row+i, col, text, functions[i]))
-    return buttons
-
-def draw_wrapped_text(stdscr, start_row, start_col, text, width, color_pair_id=None):
-    for i in range(0, len(text), width):
-        stdscr.addstr(start_row + i//width, start_col, text[i:i+width], color_pair_id)
-
 def complex_to_row_col(num):
     return (int(num.real), int(num.imag))
-
-import os
-def resize_terminal(rows, cols):
-    os.system(f'printf "\\e[8;{rows};{cols}t"')
